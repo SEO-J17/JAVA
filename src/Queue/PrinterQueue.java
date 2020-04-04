f381268ebcea44dff92feb8a2d753b42f64872ed
SEO-J17 <qw4643987@naver.com>
2020-04-04 22:48:21 +0900

    init

---
+++ src/Queue/PrinterQueue.java
@@ -0,0 +1,16 @@
+//복사를 하는데 문서의 중요도를 확인하여 중요도가 높은 문서부터 출력하도록한다.
+//따라서 어떤문서가 몇번째로 인쇄되는지를 알아내어 출력하라.
+//문서의수가 주어지고 
+//1966
+package Queue;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class PrinterQueue {
+
+	public static void main(String[] args) {
+		BufferedReader a= new BufferedReader(new InputStreamReader(System.in));
+		
+	}
+}
