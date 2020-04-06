d300567a58b8d14d2a124bf206b5a3464de6e926
SEO-J17 <qw4643987@naver.com>
2020-04-06 23:03:47 +0900

    init

---
+++ src/DFSandBFS/MapNumbering.java
@@ -0,0 +1,18 @@
+//2667 정사각형 모양의 지도가 있고 1은 집이 있고 0은 집이없는 곳을 나타낸다
+//지도의 크기를 입력 받고  총단지수를 출력하고 단지내 집수를 오름차순 정렬 하여 나타내라.
+package DFSandBFS;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+  
+public class MapNumbering {
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader a = new BufferedReader(new InputStreamReader(System.in));
+		Integer size = Integer.parseInt(a.readLine());
+		int [][] rec = new int [size+1][size+1];
+		
+		
+		
+	}
+}
