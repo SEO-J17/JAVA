c3d9e5d08a06eebafdc435b6f7866b48ea7c789a
Administrator <qw4643987@naver.com>
2021-01-05 23:32:52 +0900

    0105

--- src/문자열/그룹단어체커.java
+++ src/문자열/그룹단어체커.java
@@ -5,12 +5,28 @@ import java.util.*;
 public class 그룹단어체커{
 	public static void main(String[] args) throws NumberFormatException, IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		String str = br.readLine();
+		int n = Integer.parseInt(br.readLine());
+		int [] arr = new int [26];
+		int cnt=0;
 		
-		
-		
-		
-		
+		for(int i=0; i<n; i++) {
+			String word = br.readLine();
+			for(int j=0; j<word.length(); j++) {
+				if(j == 0) {
+					if(word.length() == 1) {
+						cnt++;
+					}else
+						arr[word.charAt(j)-'a']++;
+				}else if(word.charAt(j-1) != word.charAt(j) && arr[word.charAt(j)-'a'] != 0) {
+					break;
+				}else if(j == word.length()-1 && arr[word.charAt(j)-'a'] == 0) {
+					cnt++;
+				}else
+					arr[word.charAt(j)-'a']++;
+					
+			}
+		}
+			System.out.println(cnt);
 	   }
 	}
 
