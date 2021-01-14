5676d1fe28d3193405fd22bfb12db4fb976cc0c5
qw464 <qw464@DESKTOP-RKC1411>
2021-01-15 00:37:05 +0900

    01-15

--- src/브루트포스/일곱난쟁이.java
+++ src/브루트포스/일곱난쟁이.java
@@ -3,31 +3,42 @@ package 브루트포스;
 import java.io.*;
 import java.util.*;
 
+
 public class 일곱난쟁이 {
 	public static void main(String[] args) throws IOException, NumberFormatException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		int [] cand = new int [9];
-		int [] result = new int [7];
-		
-		for(int i=0; i<9; i++) {
-			cand[i] = Integer.parseInt(br.readLine());
-		}
-		
-		for(int i=0; i < 3; i++) {
-			int sum = cand[i];
-			for(int j=i+1; j < 9; j++) {
-				sum += result[j];
-				if(sum == 100) {
-					result[0] = cand[i];
-					break;
-				}
-					
+		StringBuilder sb = new StringBuilder();
+		int candi[] = new int [9];
+		int result[] = new int [7];
+			for (int i = 0; i < 9; i++) {
+				candi[i] = Integer.parseInt(br.readLine());
 			}
-			if(sum == 100) break;
-		}
-		
-		for(int i=1; i<7; i++) {
-		}
-		
+			
+			while(true) {
+				int sum = 0;
+				for(int i =0; i < 7; i++){
+					sum += candi[i];
+				}
+				if(sum == 100) {
+					for(int i =0; i < 7; i++){
+						result[i] = candi[i];
+					}
+					Arrays.sort(result);
+					for(int d: result)
+						sb.append(d+"\n");
+						
+						System.out.println(sb);
+						break;
+				}else {
+					for(int i=1; i < 9; i++) {
+						int tmp = candi[i-1];
+						candi[i-1] = candi[i];
+						candi[i] = tmp;
+					}
+				}
+			}
 	}
 }
+
+
+
