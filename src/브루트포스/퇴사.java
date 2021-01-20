24f6f41411675017d5a00760d5a196c5b5e0951a
qw464 <qw464@DESKTOP-RKC1411>
2021-01-21 00:21:28 +0900

    01-21

--- src/브루트포스/퇴사.java
+++ src/브루트포스/퇴사.java
@@ -3,43 +3,34 @@ package 브루트포스;
 import java.io.*;
 import java.util.*;
 public class 퇴사 {
+	static int day [];
+	static int money [];
+	static int n,max=0;
 	public static void main(String[] args) throws NumberFormatException, IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		int n = Integer.parseInt(br.readLine());
-		int arr [][] = new int [n][n];
+		n = Integer.parseInt(br.readLine());
+		day = new int [n];
+		money = new int [n];
 		
 		for(int i = 0; i < n; i++) {
 			StringTokenizer st = new StringTokenizer(br.readLine());
-			arr[i][0] = Integer.parseInt(st.nextToken());
-			arr[i][1] = Integer.parseInt(st.nextToken());
+			day[i] = Integer.parseInt(st.nextToken());
+			money[i] = Integer.parseInt(st.nextToken());
+		}
+		calc(0,0) ;
+		System.out.println(max);
+	}
+
+	private static void calc(int date, int pay) {
+		if(date >= n) {
+			max = Math.max(max, pay);
+			return;
 		}
 		
-		int max=0;
-		if( arr[n-1][0] != 1) {
-			arr[n-1][1] =0;
-		}
-		for(int i=0; i < n; i++) {
-			int sum=0;
-			int cnt=i;
-			for(int j=cnt; j < n; j++) {
-					if(cnt <= n && (arr[j][0]+cnt) <= n) {
-							sum += arr[j][1];
-							cnt += arr[j][0];
-					}else
-						 break;
-				}
-					
-			if(sum > max) {
-				max = sum;
-			}
-			if(i == n-1) {
-				System.out.print(max);
-				break;
-			}
-		}
+		if(date+day[date] <=n) calc(date+day[date], pay+money[date]);
+		else calc(date+day[date], pay);
 		
-		
-		
+		calc(date+1, pay);
 		
 	}
 }
