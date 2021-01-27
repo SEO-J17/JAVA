4d96917056e697851d0d19aac30520b799283a21
qw464 <qw464@DESKTOP-RKC1411>
2021-01-27 23:43:35 +0900

    01-27

---
+++ src/백트래킹/N과M.java
@@ -0,0 +1,14 @@
+package 백트래킹;
+import java.util.*;
+import java.io.*;
+public class N과M {
+	public static void main(String[] args) throws IOException {
+	BufferedReader br =new BufferedReader(new InputStreamReader(System.in));
+	StringTokenizer st =new StringTokenizer(br.readLine());
+	int cnt = Integer.parseInt(st.nextToken());
+	int leng = Integer.parseInt(st.nextToken());
+	qww
+	
+	
+	}
+}
