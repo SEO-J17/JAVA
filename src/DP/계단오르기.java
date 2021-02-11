08b2439403c07d966841edd75a467bafdb1e606e
qw464 <qw464@DESKTOP-RKC1411>
2021-02-11 21:11:27 +0900

    02-11

--- src/DP/계단오르기.java
+++ src/DP/계단오르기.java
@@ -1,12 +1,39 @@
 package DP;
 import java.util.*;
+
 import java.io.*;
 public class 계단오르기 {
-
+	static int arr[];
+	static Integer result[];
 	public static void main(String[] args) throws NumberFormatException, IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		int n = Integer.parseInt(br.readLine());
+		 int n = Integer.parseInt(br.readLine());
+		arr = new int [n+1];
+		result = new Integer [n+1];
 		
+		for(int i=1; i<=n; i++)
+			arr[i] = Integer.parseInt(br.readLine());
 		
+		result[0] = arr[0];
+		result[1] = arr[1];
+		
+		//n이 1이 입력될수도 있기에!
+		if(n>=2) {
+			result[2] = arr[1]+arr[2];
+		}
+		
+		System.out.println(calc(n));
+		
+	}
+
+	
+	private static int calc(int n) {
+		
+		if(result[n] == null) {
+			result[n] = Math.max(calc(n-2),calc(n-3)+arr[n-1])+arr[n];
+			
+		}
+		
+		return result[n];
 	}
 }
---
+++ src/DP/일로만들기.java
@@ -0,0 +1,23 @@
+//1463
+package DP;
+import java.util.*;
+import java.io.*;
+public class 일로만들기 {
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int n = Integer.parseInt(br.readLine());
+		int arr[] = new int [n+1];
+		
+		arr[0]=0;
+		arr[1]=0;
+		
+	    for(int i=2; i<=n; i++) {
+            arr[i] = arr[i-1]+1; 
+            if(i%2 == 0) arr[i] = Math.min(arr[i/2]+1, arr[i]); 
+            if(i%3 == 0) arr[i] = Math.min(arr[i/3]+1, arr[i]);
+        }
+        
+	    System.out.println(arr[n]);
+
+	}
+}
