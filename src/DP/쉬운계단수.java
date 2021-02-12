b382226ec5aa549c5e67141daa4761458cb3b69b
SEO-J17 <qw4643987@naver.com>
2021-02-13 01:51:24 +0900

    02-13

--- src/DP/쉬운계단수.java
+++ src/DP/쉬운계단수.java
@@ -3,17 +3,41 @@ package DP;
 import java.io.*;
 import java.util.*;
 public class 쉬운계단수 {
+	static Long arr[][];
+	static int n;
 	public static void main(String[] args) throws NumberFormatException, IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		int n = Integer.parseInt(br.readLine());
+		n = Integer.parseInt(br.readLine());
+		arr = new Long [n+1][10];
 		
-		if(n ==1) {
-			System.out.println(9);
-		}else {
-			
-			
-			
+		for(int i=0; i < 10; i++ ) {
+			arr[1][i] = (long)1;
 		}
 		
+		long sum=0;
+		for(int i=1; i<10; i++) {
+			sum += calc(n,i);
+		}
+		
+		System.out.println(sum%1000000000);
+			
+	}
+	
+	
+	private static long calc(int num, int value) {
+		if(num == 1) {
+			return arr[num][value];
+		}
+		
+		if(arr[num][value] == null) {
+			if(value == 0) {
+				arr[num][value] = calc(num-1, 1);
+			}else if(value == 9) {
+				arr[num][value] = calc(num-1, 8);
+			}else
+				arr[num][value] = calc(num-1, value-1) + calc(num-1, value+1) ;
+		}
+		
+		return arr[num][value]%1000000000;
 	}
 }
