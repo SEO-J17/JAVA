c676a9a5e4fdd97b864da5b7419d5c2592fddd4c
SEO-J17 <qw4643987@naver.com>
2021-02-13 01:33:53 +0900

    02-13

---
+++ src/DP/쉬운계단수.java
@@ -0,0 +1,19 @@
+//10844
+package DP;
+import java.io.*;
+import java.util.*;
+public class 쉬운계단수 {
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int n = Integer.parseInt(br.readLine());
+		
+		if(n ==1) {
+			System.out.println(9);
+		}else {
+			
+			
+			
+		}
+		
+	}
+}
