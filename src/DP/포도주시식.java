297855eca3a2bc1d094c47b6842a2b78f223c032
SEO-J17 <qw4643987@naver.com>
2021-02-13 21:20:21 +0900

    02-13

--- src/DP/포도주시식.java
+++ src/DP/포도주시식.java
@@ -3,9 +3,15 @@ package DP;
 import java.util.*;
 import java.io.*;
 public class 포도주시식 {
-	public static void main(String[] args) {
+	static int arr[];
+	public static void main(String[] args) throws NumberFormatException, IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
+		int n = Integer.parseInt(br.readLine());
+		arr = new int [n];
+		for(int i=0; i<n; i++) {
+			arr[i] = Integer.parseInt(br.readLine());
+		}
+		
 		
 		
 		
