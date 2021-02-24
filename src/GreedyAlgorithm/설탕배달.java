440aae5d6c234937e294ce05f48b2014c70a76b7
SEO-J17 <qw4643987@naver.com>
2021-02-24 19:40:06 +0900

    02-24

---
+++ src/GreedyAlgorithm/설탕배달.java
@@ -0,0 +1,24 @@
+package GreedyAlgorithm;
+import java.io.*;
+import java.util.*;
+public class º≥≈¡πË¥ﬁ {
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int n = Integer.parseInt(br.readLine());
+		int cnt=0;
+		
+		while(true) {
+			 if(n%5 == 0) {
+				 cnt += n/5;
+				 System.out.println(cnt);
+				 break;
+			 }else if (n < 0) {
+				 System.out.println(-1);
+				 break;
+			 }else {
+				 n -= 3;
+				 cnt++;
+			 }
+		}
+	}
+}
