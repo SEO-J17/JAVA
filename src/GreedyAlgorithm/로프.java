33997e03fd71c8e7bd75c762eb88c795e650043e
SEO-J17 <qw4643987@naver.com>
2021-02-26 18:59:32 +0900

    02-26

---
+++ src/GreedyAlgorithm/로프.java
@@ -0,0 +1,35 @@
+package GreedyAlgorithm;
+import java.io.*;
+import java.util.*;
+public class 로프 {
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int n = Integer.parseInt(br.readLine());
+		Integer arr [] = new Integer [n];
+		int max = 0;
+	
+		if(n ==1) {
+			System.out.println(Integer.parseInt(br.readLine()));
+		}else {
+			for(int i=0; i<n; i++) {
+				arr[i] = Integer.parseInt(br.readLine());
+			}
+			
+			Arrays.sort(arr, Collections.reverseOrder());
+		
+			max = 2*arr[1];
+	
+			for(int i=2; i<n; i++) {
+				int temp = (i+1)*arr[i];
+				if(max < temp) {
+					max = temp;
+				}
+			}
+			
+			System.out.println(max);
+			
+		}
+	}
+}
+
+//역정렬은 integer배열만 가능!
