f11342c1630bf06cc06ee7318bef4d03c8ee5376
SEO-J17 <qw4643987@naver.com>
2021-03-05 03:05:01 +0900

    03-05

---
+++ src/GreedyAlgorithm/신입사원.java
@@ -0,0 +1,34 @@
+package GreedyAlgorithm;
+import java.io.*;
+import java.util.*;
+public class 신입사원 {
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		int n = Integer.parseInt(br.readLine());
+		
+		while(n --> 0) {
+			int num = Integer.parseInt(br.readLine());
+			int [] grade = new int [num+1];
+			int max=1;
+			
+			for(int i=0; i<num; i++) {
+				StringTokenizer st = new StringTokenizer(br.readLine());
+				int a = Integer.parseInt(st.nextToken());
+				int b = Integer.parseInt(st.nextToken());
+				grade[a] = b;
+			}
+			
+			int temp = grade[1];
+			for(int i=2; i<=num; i++) {
+				if(temp > grade[i]) {
+					max++;
+					temp = grade[i];
+				}
+			}
+			sb.append(max).append("\n");
+			}
+		
+		System.out.println(sb);
+		}
+}
