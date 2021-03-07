2ba99e09f23e2ae9371979ca1917050fa91fffd3
SEO-J17 <qw4643987@naver.com>
2021-03-08 02:36:52 +0900

    03-08

---
+++ src/BFS/미로탐색.java
@@ -0,0 +1,21 @@
+package BFS;
+import java.io.*;
+import java.util.*;
+public class ¹Ì·ÎÅ½»ö {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int n = Integer.parseInt(st.nextToken());
+		int m = Integer.parseInt(st.nextToken());
+		
+		int arr[][] = new int [n+1][m+1];
+		for(int i=1; i<n; i++) {
+			int temp = new 
+			for(int j=0; j<m; j++) {
+				
+			}
+		}
+		
+		
+	}
+}
