9bb2cdd18f42c654fbd80ed342a711536721158b
SEO-J17 <qw4643987@naver.com>
2021-03-09 00:25:13 +0900

    03-09

--- src/BFS/단지번호붙이기.java
+++ src/BFS/단지번호붙이기.java
@@ -1,10 +1,11 @@
 package BFS;
-
+import java.io.*;
+import java.util.*;
 public class 단지번호붙이기 {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int n = Integer.parseInt(br.readLine());
+		
+		
 	}
-
 }
--- src/DP/일이삼더하기.java
+++ src/DP/일이삼더하기.java
@@ -1,10 +1,11 @@
 package DP;
-
+import java.util.*;
+import java.io.*;
 public class 일이삼더하기 {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int n = Integer.parseInt(br.readLine());
+		
+		
 	}
-
 }
