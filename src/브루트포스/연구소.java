dc3ad78919cde736e8ae2ce3caea5a2e1bec4249
SEO-J17 <qw4643987@naver.com>
2021-07-18 01:48:43 +0900

    07-18

--- src/브루트포스/연구소.java
+++ src/브루트포스/연구소.java
@@ -3,8 +3,22 @@ import java.util.*;
 import java.io.*;
 
 public class ¿¬±¸¼Ò {
-	public static void main(String[] args) {
+	static int arr [][];
+	static int cnt = 0;
+	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int n = Integer.parseInt(st.nextToken());
+		int m = Integer.parseInt(st.nextToken());
+		arr = new int [n][m];
+		
+		for(int i = 0; i < n; i++) {
+			for(int j = 0; j < m; j++) {
+				arr[i][j] = Integer.parseInt(br.readLine());
+			}
+		}
+		
+		
 		
 	}
 }
