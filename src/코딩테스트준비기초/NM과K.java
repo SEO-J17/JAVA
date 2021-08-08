2249d7166d29a25de34bf7741170933f43b3640a
SEO-J17 <qw4643987@naver.com>
2021-08-09 00:18:09 +0900

    08-09

---
+++ src/코딩테스트준비기초/NM과K.java
@@ -0,0 +1,17 @@
+package 코딩테스트준비기초;
+
+import java.io.*;
+import java.util.*;
+
+public class NM과K {
+	static int n, m, k;
+	static int dx[] = { 1, -1, 0, 0 };
+	static int dy[] = { 0, 0, 1, -1 };
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		
+	}
+}
---
+++ src/코딩테스트준비기초/N과M3_15651.java
@@ -0,0 +1,37 @@
+package 코딩테스트준비기초;
+
+import java.io.*;
+import java.util.*;
+
+public class N과M3_15651 {
+	static int n, m;
+	static int arr[];
+	static StringBuilder sb = new StringBuilder();
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		n = Integer.parseInt(st.nextToken());
+		m = Integer.parseInt(st.nextToken());
+		arr = new int [m];
+		dfs(0);
+
+		System.out.print(sb);
+	}
+
+	private static void dfs(int depth) {
+		if (depth == m) {
+			for (int d : arr)
+				sb.append(d).append(" ");
+
+			sb.append("\n");
+			return;
+		}
+
+		for (int i = 1; i <= n; i++) {
+			arr[depth] = i;
+			dfs(depth + 1);
+		}
+
+	}
+}
