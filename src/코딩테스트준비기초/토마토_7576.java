7dca0b47b7cf052cae1402f32d3303be52054160
SEO <qw4643987@naver.com>
2021-09-27 18:18:13 +0900

    rename and not done

---
+++ src/코딩테스트준비기초/토마토_7576.java
@@ -0,0 +1,83 @@
+package 코딩테스트준비기초;
+
+import java.io.*;
+import java.util.*;
+
+public class 토마토_7576 {
+	static int n, m, day = 0;
+	static int arr[][];
+	static int dx[] = { 0, 0, -1, 1 };
+	static int dy[] = { 1, -1, 0, 0 };
+	static Queue<Dot> qu = new LinkedList<>();
+
+	static class Dot {
+		int x;
+		int y;
+		int cnt;
+
+		public Dot(int x, int y, int cnt) {
+			this.x = x;
+			this.y = y;
+			this.cnt = cnt;
+		}
+	}
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int n = Integer.parseInt(st.nextToken());
+		int m = Integer.parseInt(st.nextToken());
+
+		arr = new int[m][n];
+
+		for (int i = 0; i < m; i++) {
+			st = new StringTokenizer(br.readLine());
+			for (int j = 0; j < n; j++) {
+				arr[i][j] = Integer.parseInt(st.nextToken());
+				if (arr[i][j] == 1) {
+					qu.offer(new Dot(i, j, 0));
+				}
+			}
+		}
+
+		calc();
+
+	}
+
+	private static void calc() {
+
+		while (!qu.isEmpty()) {
+			Dot dot = qu.poll();
+			day = dot.cnt;
+			for (int i = 0; i < 4; i++) {
+				int xx = dot.x + dx[i];
+				int yy = dot.y + dy[i];
+				if (xx >= 0 && yy >= 0 && xx < m && yy < n) {
+					if (arr[xx][yy] == 0) {
+						arr[xx][yy] = 1;
+						qu.add(new Dot(xx, yy, day + 1));
+					}
+				}
+			}
+		}
+
+		if (chktomato()) {
+			System.out.println(day);
+		} else
+			System.out.println(-1);
+
+	}
+
+	private static boolean chktomato() {
+		for (int i = 0; i < m; i++) {
+			for (int j = 0; j < n; j++) {
+				if (arr[i][j] == 0) {
+					return false;
+				}
+			}
+		}
+
+		return true;
+
+	}
+}
