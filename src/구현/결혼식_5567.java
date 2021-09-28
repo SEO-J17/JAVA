35a82a07f88ee10c5070a73a8e74b9d342d55fa0
SEO-J17 <qw4643987@naver.com>
2021-09-28 23:43:40 +0900

    edit 09-28

--- src/구현/결혼식_5567.java
+++ src/구현/결혼식_5567.java
@@ -4,19 +4,14 @@ import java.io.*;
 import java.util.*;
 
 public class 결혼식_5567 {
-	static int n, m; // m = 리스트의 길이
-	static int arr[][];
-	static boolean chk[];
-	static boolean visit[];
-
 	public static void main(String[] args) throws NumberFormatException, IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		n = Integer.parseInt(br.readLine());
-		m = Integer.parseInt(br.readLine());
+		int n = Integer.parseInt(br.readLine());
+		int m = Integer.parseInt(br.readLine());
 
-		arr = new int[n + 1][n + 1];
-		chk = new boolean[n + 1];
-		visit = new boolean[n + 1];
+		int arr[][] = new int[n + 1][n + 1];
+		boolean chk[] = new boolean[n + 1];
+		boolean visit[] = new boolean[n + 1];
 		
 		for (int i = 0; i < m; i++) {
 			StringTokenizer st = new StringTokenizer(br.readLine());
@@ -30,6 +25,7 @@ public class 결혼식_5567 {
 		qu.add(1);
 		chk[1] = true;
 		visit[1] = true;
+		
 		while (!qu.isEmpty()) {
 			int start = qu.poll();
 			for (int i = 1; i <= n; i++) {
