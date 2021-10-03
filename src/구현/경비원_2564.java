9f250084e48c62d16755fdf5b08c1db1c8c64dde
SEO-J17 <qw4643987@naver.com>
2021-10-03 23:16:08 +0900

    10-03 done

--- src/구현/경비원_2564.java
+++ src/구현/경비원_2564.java
@@ -4,61 +4,38 @@ import java.io.*;
 import java.util.*;
 
 public class 경비원_2564 {
-	static int n, m, tc;
-	static int arr[][];
-	static int x[][];
-	static int stand;
-	static int sum=0;
-
-	// static int left_cnt = 0 ,right_cnt;
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
-		n = Integer.parseInt(st.nextToken());
-		m = Integer.parseInt(st.nextToken());
-		tc = Integer.parseInt(br.readLine());
-		arr = new int[n][m];
-		x = new int[1][2];
+		int n = Integer.parseInt(st.nextToken());
+		int m = Integer.parseInt(st.nextToken());
+		int tc = Integer.parseInt(br.readLine());
+		int sum = 0;
+		ArrayList<Integer> list = new ArrayList<>();
 
-		for (int i = 0; i < tc; i++) {
+		for (int i = 0; i <= tc; i++) {
 			st = new StringTokenizer(br.readLine());
-			for (int j = 0; j < 2; j++) {
-				arr[i][j] = Integer.parseInt(st.nextToken());
+			int direc = Integer.parseInt(st.nextToken());
+			int pos = Integer.parseInt(st.nextToken());
+			if (direc == 1) {
+				list.add(pos);
+			} else if (direc == 2) {
+				list.add(n + m + n - pos);
+			} else if (direc == 3) {
+				list.add(2 * n + m + m - pos);
+			} else {
+				list.add(n + pos);
 			}
 		}
-		st = new StringTokenizer(br.readLine());
-		x[0][0] = Integer.parseInt(st.nextToken());
-		x[0][1] = Integer.parseInt(st.nextToken());
 
-		calc();
+		int x = list.get(list.size() - 1);
+		int rec_length = 2 * (n + m);
+
+		for (int i = 0; i < tc; i++) {
+			int dis = Math.abs(list.get(i) - x);
+			sum += Math.min(dis, rec_length - dis);
+		}
 
 		System.out.println(sum);
-	}
-
-	private static void calc() {
-
-		for (int i = 0; i < tc; i++) {
-			if (x[0][0] == 1 || x[0][0] == 2) { // 가로에 있다면
-				if (x[0][0] == arr[i][0]) {
-					sum += Math.abs(x[0][1] - arr[i][1]);
-				} else if (arr[i][0] == 3) {
-					sum += Math.min(x[0][1] + arr[i][1], (n - x[0][1]) + m + n + (m - arr[i][1]));
-				} else if (arr[i][0] == 4) {
-					sum += Math.min(x[0][1] + m + n + (m - arr[i][1]), (n - x[0][1]) + arr[i][1]);
-				} else {
-					sum += Math.min(x[0][1] + m + arr[i][1], (n - x[0][1]) + m + n - arr[i][1]);
-				}
-			} else { // 세로에 있다면
-				if (x[0][0] == arr[i][0]) {
-					sum += Math.abs(x[0][1] - arr[i][1]);
-				} else if (arr[i][0] == 1) {
-					sum += Math.min(x[0][1] + n - arr[i][1], (m - x[0][1]) + n + m + arr[i][1]);
-				} else if (arr[i][0] == 2) {
-					sum += Math.min(m - x[0][1] + (n - arr[i][1]), x[0][1] + n + m + arr[i][1]);
-				} else {
-					sum += Math.min(x[0][1] + n + arr[i][1], (m - x[0][1]) + n + (m - arr[i][1]));
-				}
-			}
-		}
 	}
 }
