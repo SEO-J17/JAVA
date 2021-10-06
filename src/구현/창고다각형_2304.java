30d42ddff5d60eb52c86e31f320eb31831a2954d
SEO <qw4643987@naver.com>
2021-10-06 16:23:44 +0900

    10-06 refactoring done

--- src/구현/창고다각형_2304.java
+++ src/구현/창고다각형_2304.java
@@ -4,10 +4,7 @@ import java.io.*;
 import java.util.*;
 
 public class 창고다각형_2304 {
-	static int sum;
 	static int y[];
-	static int end = -1;
-	static int tall;
 
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -16,6 +13,10 @@ public class 창고다각형_2304 {
 
 		int start = 999;
 		int temp = -1;
+		int end = -1;
+		int tall = -1;
+		int sum = 0;
+
 		for (int i = 0; i < n; i++) {
 			StringTokenizer st = new StringTokenizer(br.readLine());
 			int xx = Integer.parseInt(st.nextToken());
@@ -35,26 +36,27 @@ public class 창고다각형_2304 {
 		sum = y[start];
 		int standard = y[start];
 
-		for (int i = start + 1; i <= end; i++) {
-			if (i == tall) { 		// 만약 제일 긴 길이에 도달 했다면 더하기를 중지하고 뒤로 더하기를 실시한다.
+		for (int i = start + 1; i <= tall; i++) {
+			if (i == tall) {	 // 만약 제일 긴 길이에 도달 했다면 더하기를 중지한다.
 				sum += y[i];
-				backcalc(); 		// 뒤부터 계산을 시작하는 메소드
 				break;
 			}
-			if (standard < y[i]) { 		// 기준의 길이보다 길면 기준값을 더 긴값으로 바꿈.
+			if (standard < y[i]) { // 기준의 길이보다 길면 기준값을 더 긴값으로 바꿈.
 				sum += y[i];
 				standard = y[i];
-			} else if (standard >= y[i]) { 		// 기준이 길이가 더 길면 그냥 더하기.
+			} else if (standard >= y[i]) { // 기준이 길이가 더 길면 그냥 더하기.
 				sum += standard;
 			}
 		}
-
-		System.out.println(sum);
+		
+		//backcalc 라는 메소드를 통해서 맨 뒤부터 가장 긴 길이의 x좌표 앞까지 계산을 한다. 
+		System.out.print(backcalc(sum, end, tall));
 
 	}
 
-	private static void backcalc() {
+	private static int backcalc(int sum, int end, int tall) {
 		int standard = y[end];
+		
 		for (int i = end; i > tall; i--) {
 			if (standard < y[i]) {
 				standard = y[i];
@@ -63,5 +65,8 @@ public class 창고다각형_2304 {
 				sum += standard;
 			}
 		}
+		
+		return sum;
+		
 	}
 }
