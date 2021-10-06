122a344c9231a9a06ade0d24193ebc3a8b7a5d65
SEO <qw4643987@naver.com>
2021-10-06 16:07:17 +0900

    10-06 solved

--- src/구현/창고다각형_2304.java
+++ src/구현/창고다각형_2304.java
@@ -5,7 +5,6 @@ import java.util.*;
 
 public class 창고다각형_2304 {
 	static int sum;
-	static int x[];
 	static int y[];
 	static int end = -1;
 	static int tall;
@@ -13,22 +12,20 @@ public class 창고다각형_2304 {
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int n = Integer.parseInt(br.readLine());
-		x = new int[1001];
 		y = new int[1001];
-		
+
 		int start = 999;
 		int temp = -1;
 		for (int i = 0; i < n; i++) {
 			StringTokenizer st = new StringTokenizer(br.readLine());
 			int xx = Integer.parseInt(st.nextToken());
 			int yy = Integer.parseInt(st.nextToken());
-			x[xx] = 1;
 			y[xx] = yy;
-			
-			//시작점과 끝점 찾기
+
+			// 시작점과 끝점 찾기
 			start = Math.min(xx, start);
 			end = Math.max(xx, end);
-			//가장 길이가 긴 x좌표 구하기
+			// 가장 길이가 긴 x좌표 구하기
 			if (temp < yy) {
 				temp = yy;
 				tall = xx;
@@ -37,17 +34,17 @@ public class 창고다각형_2304 {
 
 		sum = y[start];
 		int standard = y[start];
-		
+
 		for (int i = start + 1; i <= end; i++) {
-			if (i == tall) {			//만약 제일 긴 길이에 도달 했다면 더하기를 중지하고 뒤로 더하기를 실시한다.
+			if (i == tall) { 		// 만약 제일 긴 길이에 도달 했다면 더하기를 중지하고 뒤로 더하기를 실시한다.
 				sum += y[i];
-				backcalc();				//뒤부터 계산을 시작하는 메소드
+				backcalc(); 		// 뒤부터 계산을 시작하는 메소드
 				break;
 			}
-			if (standard < y[i]) {		//기준의 길이보다 길면 기준값을 더 긴값으로 바꿈.
+			if (standard < y[i]) { 		// 기준의 길이보다 길면 기준값을 더 긴값으로 바꿈.
 				sum += y[i];
 				standard = y[i];
-			} else if (standard >= y[i]) {		//기준이 길이가 더 길면 그냥 더하기.
+			} else if (standard >= y[i]) { 		// 기준이 길이가 더 길면 그냥 더하기.
 				sum += standard;
 			}
 		}
