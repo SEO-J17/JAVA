ebf991a8cc6eef49da0b9ecf1a774dbe7636ed7c
SEO-J17 <qw4643987@naver.com>
2022-01-24 19:34:11 +0900

    01-24 solved

--- src/DFS/촌수계산.java
+++ src/DFS/촌수계산.java
@@ -1,50 +1,56 @@
 //2644
 package DFS;
+
 import java.util.*;
 import java.io.*;
+
 public class 촌수계산 {
 	static int n;
 	static int arr[][];
-	static int man1,man2;
-	static int num;
+	static boolean visit[];
+	static int man1, man2;
+	static int cnt = -1;
+
 	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));	
-		n = Integer.parseInt(br.readLine());		//사람수 입력
-		arr = new int [n+1][n+1];
-		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		n = Integer.parseInt(br.readLine()); // 사람수 입력
 		StringTokenizer st = new StringTokenizer(br.readLine());
 		man1 = Integer.parseInt(st.nextToken());
 		man2 = Integer.parseInt(st.nextToken());
-		
-		int m = Integer.parseInt(br.readLine());	//관계개수
-		
-		for(int i = 0; i < m; i++) {
+
+		arr = new int[n + 1][n + 1];
+		visit = new boolean[n + 1];
+		int num = Integer.parseInt(br.readLine());
+
+		for (int i = 0; i < num; i++) {
 			st = new StringTokenizer(br.readLine());
 			int a1 = Integer.parseInt(st.nextToken());
 			int a2 = Integer.parseInt(st.nextToken());
 			arr[a1][a2] = arr[a2][a1] = 1;
 		}
-		
-		
-		
-		
+
+		calc(man1, 0);
+
+		if (cnt > 0) {
+			System.out.println(cnt);
+		} else
+			System.out.println(-1);
+
 	}
+
 	
-	private static void bfs(int man_1, int man_2) {
-		boolean[] visit = new boolean [n+1];
-		Queue<Integer> qu = new LinkedList<>();
-		visit[man_1] = true;
-		qu.add(man_1);
-		
-		while(!qu.isEmpty()) {
-				int now = qu.poll();
-				for(int j = 1; j <= n; j++) {
-					if(arr[now][j] == 1 && !visit[j]) {
-						visit[j] = true;
-						qu.add(j);
-					}
+	private static void calc(int node, int depth) {
+		visit[node] = true;
+		if (node == man2) {
+			cnt = depth;
+			return;
+		}
+		for (int i = 1; i <= n; i++) {
+			if (visit[i] != true) {
+				if (arr[node][i] == 1) {
+					calc(i, depth + 1);
 				}
-			num++;
+			}
 		}
 	}
 }
