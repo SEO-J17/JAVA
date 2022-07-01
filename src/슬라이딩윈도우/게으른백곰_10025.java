6d583cee173b2937b414194c5da66ecc87c2eb1c
SEO-J17 <qw4643987@naver.com>
2022-07-01 17:28:29 +0900

    solved 07-01

--- src/슬라이딩윈도우/게으른백곰_10025.java
+++ src/슬라이딩윈도우/게으른백곰_10025.java
@@ -9,12 +9,28 @@ public class ∞‘¿∏∏•πÈ∞ı_10025 {
 		StringTokenizer st = new StringTokenizer(br.readLine());
 		int n = Integer.parseInt(st.nextToken());
 		int k = Integer.parseInt(st.nextToken());
-		
-		for(int i = 0; i < n; i++) {
-			
+
+		int arr[] = new int[1000001];
+		for (int i = 1; i <= n; i++) {
+			st = new StringTokenizer(br.readLine());
+			int temp1 = Integer.parseInt(st.nextToken());
+			int temp2 = Integer.parseInt(st.nextToken());
+			arr[temp2] += temp1;
 		}
-		
-		
-		
+
+		int sum = 0;
+		int max = 0;
+		int lim = 1 + (2 * k);
+		for (int i = 0; i <= 1000000; i++) {
+			if (i >= lim) {
+				sum -= arr[i - lim];
+			}
+
+			sum += arr[i];
+			max = Math.max(max, sum);
+		}
+
+		System.out.println(max);
+
 	}
 }
